a8fd5549718d5fd2ec43b3955d9caa963032bc27
Wieland Hilker <60106690+AlphaMoonbaseBerlin@users.noreply.github.com>
2025-08-25 18:42:21 +0200

    Update __init__.py

--- src/TauCeti/__init__.py
+++ src/TauCeti/__init__.py
@@ -3,4 +3,4 @@ from typing import Literal
 
 # Lets check if this can be automated or if we need to do some more manual work
 def FetchTox( path:Literal["PresetCuelist", "PresetDashboard", "PresetManager", "PresetMapper", "Tweener"] ):
-    return import_module( path ).ToxFile
+    return import_module( f".{path}", "TauCeti" ).ToxFile
