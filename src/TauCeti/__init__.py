3ea261e5c4de4281b1a986f58d60573592a22a92
Wieland Hilker <60106690+AlphaMoonbaseBerlin@users.noreply.github.com>
2025-08-25 19:52:52 +0200

    util collection and comments. All hail the package.

--- src/TauCeti/__init__.py
+++ src/TauCeti/__init__.py
@@ -1,6 +1,19 @@
 from importlib import import_module
 from typing import Literal
 
-# Lets check if this can be automated or if we need to do some more manual work
+
+
 def FetchTox( path:Literal["PresetCuelist", "PresetDashboard", "PresetManager", "PresetMapper", "Tweener"] ):
+    """
+        A helper function to enable importing nested modules in TouchDesigner using Module on Demand. 
+        TouchDesigners Module on Deman (mod) can not import nested packages and will fail. 
+        While you can run 
+        from TauCeti.PresetCuelist import ToxFile
+        in any script, running
+        mod.TauCeti.PresetCuelist.ToxFile
+        will fail. Instead you need to use this helper-function
+        mod.TauCeti.FetchTox("PresetCuelist")
+
+        Still evaluating is there is a better way, but this is close enough.
+    """
     return import_module( f".{path}", "TauCeti" ).ToxFile
