bec104ef40ab4e5836fc836d80a682b8e723c361
Wieland Hilker <60106690+AlphaMoonbaseBerlin@users.noreply.github.com>
2025-10-18 23:24:23 +0200

    release script.

---
+++ AppData/Scripts/package_release.py
@@ -0,0 +1,29 @@
+ 
+search_tag = "package_release_candidate"
+
+for target in root.findChildren( tags = [search_tag]):
+    
+    target.tags.remove(search_tag)
+    target.tags.remove( op("PrivateInvestigator").par.Tag.eval() )
+    
+    prereleasescript = target.op("pre_release")
+    if prereleasescript is not None: prereleasescript.run()
+    op("PrivateInvestigator").Save( target )
+    op("PrivateInvestigator").Release( target )
+
+
+from subprocess import call
+# call("uv version --bump minor")
+
+import tomllib
+with open("pyproject.toml", "rb") as projecttoml:
+    projectdata = tomllib.load( projecttoml )
+    version = projectdata["project"]["version"]
+
+call(f'git checkout -b v{version}')
+call(f'git commit . -m "TauCeti Release v{version}" ')
+# call(f'git tag -a v{version} -m "TauCeti Release v{version}"')
+
+
+
+
