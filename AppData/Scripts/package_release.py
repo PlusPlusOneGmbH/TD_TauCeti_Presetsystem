a1e44fd405928e23f0ce3b5715338fd8a45bb1f7
Wieland Hilker <60106690+AlphaMoonbaseBerlin@users.noreply.github.com>
2025-10-20 21:29:46 +0200

    releasescript adjustments.

--- AppData/Scripts/package_release.py
+++ AppData/Scripts/package_release.py
@@ -1,30 +1,39 @@
  
-search_tag = "package_release_candidate"
-
-for target in parent.Project.findChildren( tags = [search_tag] ):
-    
-    target.tags.remove(search_tag)
-    for child in list( target.findChildren( tags = op("PrivateInvestigator").par.Tag.eval()) ) + [target]:
-        child.tags.remove( op("PrivateInvestigator").par.Tag.eval() )
-    
-    prereleasescript = target.op("pre_release")
-    if prereleasescript is not None: prereleasescript.run()
-    op("PrivateInvestigator").Save( target )
-    op("PrivateInvestigator").Release( target )
-
 
 from subprocess import call
-call("uv version --bump patch")
+
+search_tag = "package_release_candidate"
+release_candidates = parent.Project.findChildren( tags = [search_tag] )
+
+# Lets release the proper dist to the actual repository for everyone to be able to fetch them directly without having to 
+# select the correct branch.
+for target in release_candidates:
+    target.tags.remove(search_tag)
+    op("PrivateInvestigator").Release( target )
 
 import tomllib
 with open("pyproject.toml", "rb") as projecttoml:
     projectdata = tomllib.load( projecttoml )
     version = projectdata["project"]["version"]
 
+call("uv version --bump patch")
+call(f'git commit . -m "Bump to Version {version}"')
+
+# Now lets prepare everything for a clean buildprocess.
+# In the future all of this will run outside ;)
+
+for target in release_candidates:
+    for child in list( target.findChildren( tags = [op("PrivateInvestigator").par.Tag.eval()] ) ) + [target]:
+        debug(f"Removing tag from {child}")
+        child.tags.remove( op("PrivateInvestigator").par.Tag.eval() )
+    
+    prereleasescript = target.op("pre_release")
+    if prereleasescript is not None: prereleasescript.run()
+    op("PrivateInvestigator").Save( target )
+
 call(f'git checkout -b v{version}')
 call("git add .")
 call(f'git commit . -m "TauCeti Release v{version}" ')
-# call(f'git tag -a v{version} -m "TauCeti Release v{version}"')
 
 
 
