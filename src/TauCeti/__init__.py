d37151e1e5fb5c29191c695902b16ba1b4bf0c99
Wieland Hilker <60106690+AlphaMoonbaseBerlin@users.noreply.github.com>
2025-10-21 15:16:39 +0200

    Update __init__.py

--- src/TauCeti/__init__.py
+++ src/TauCeti/__init__.py
@@ -1,6 +1,9 @@
+# Makre sure subpackages are imported so they can be accesses via the mod. method.
 from . import PresetManager
 from . import Tweener
 
+# Future Proofing
+__minimum_td_version__ = "2023.1200"
 
 # Futureprrofing for automated search of toxfiles and imports.
 _ToxFiles = {
